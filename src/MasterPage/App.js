9c7b0ae612a14b79dcc53def6d39546db3ac7c05
linlijian <praw__Rul2__lvluk@Outlook.co.th>
2019-06-22 22:09:33 +0700

    #4 state and set state

--- src/MasterPage/App.js
+++ src/MasterPage/App.js
@@ -5,14 +5,32 @@ import '../Person/Person';
 import Person from '../Person/Person';
 
 class App extends Component {
+  // this state area
+  state = {
+    name: 'A',
+    age: 23,
+    person: [
+      { name: 'Meiio', age: 19 },
+      { name: 'Pori', age: 9 },
+      { name: 'Kiro', age: 10 }
+    ]
+  }
 
+  onClickChnageState = () => {
+    this.setState({ name: 'Ong' })
+  }
+  
   render() {
 
     return (
       <div>
         <p>Hello world</p>
-        <Person name = {'A'}/>
+        <Person name={'A'} />
         <Person >props children</Person >
+
+        <Person name={this.state.name} >{this.state.age}</Person >
+        <Person name={this.state.person[0].name} >{this.state.person[0].age}</Person >
+        <p onClick={this.onClickChnageState}>Click Change State</p>
       </div>
     );
   }
